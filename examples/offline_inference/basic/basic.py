396e4a746f3c6fc224983062198a97379cc50347
Sylvie Liberman <sylv@h100-80gb-sxm-ib-8x-speedysnail.cloud.together.ai>
2025-05-15 13:54:11 -0700

    with debugging

--- examples/offline_inference/basic/basic.py
+++ examples/offline_inference/basic/basic.py
@@ -3,10 +3,11 @@ from vllm import LLM, SamplingParams
 
 # Sample prompts.
 prompts = [
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
-    "The future of AI is",
+    "a " * 100 # should be ~103 tokens
+    #"Hello, my name is", # 7 tokens
+    # "The president of the United States is",
+    # "The capital of France is",
+    # "The future of AI is",
 ]
 # Create a sampling params object.
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=32)
@@ -14,7 +15,8 @@ sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=32)
 
 def main():
     # Create an LLM.
-    llm = LLM(model="facebook/opt-125m", enforce_eager=True)
+    # llm = LLM(model="facebook/opt-125m", enforce_eager=True)
+    llm = LLM(model="meta-llama/Llama-3.2-1B", enforce_eager=True)
     # Generate texts from the prompts.
     # The output is a list of RequestOutput objects
     # that contain the prompt, generated text, and other information.
