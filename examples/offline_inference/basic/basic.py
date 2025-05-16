3b1647a997be52fd5587930bec95b2c6f738cd1f
Sylvie Liberman <sylv@h100-80gb-sxm-ib-8x-speedysnail.cloud.together.ai>
2025-05-16 13:52:42 -0700

    reshape cache in interface

--- examples/offline_inference/basic/basic.py
+++ examples/offline_inference/basic/basic.py
@@ -3,20 +3,22 @@ from vllm import LLM, SamplingParams
 
 # Sample prompts.
 prompts = [
+# "My",
+# "My name"
     "a " * 100 # should be ~103 tokens
-    #"Hello, my name is", # 7 tokens
+    # "Hello, my name is", # 7 tokens
     # "The president of the United States is",
     # "The capital of France is",
     # "The future of AI is",
 ]
 # Create a sampling params object.
-sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=32)
+sampling_params = SamplingParams(temperature=0, top_p=0.95, max_tokens=32)
 
 
 def main():
     # Create an LLM.
     # llm = LLM(model="facebook/opt-125m", enforce_eager=True)
-    llm = LLM(model="meta-llama/Llama-3.2-1B", enforce_eager=True, tensor_parallel_size=8)
+    llm = LLM(model="meta-llama/Llama-3.2-1B", enforce_eager=True, tensor_parallel_size=8, block_size=256)
     # Generate texts from the prompts.
     # The output is a list of RequestOutput objects
     # that contain the prompt, generated text, and other information.
